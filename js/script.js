6f89150069912e0d2a2d5eb01154265cf89ba67a
Stacey Graham <35972972+StashaG@users.noreply.github.com>
2019-11-20 17:12:30 -0500

    created a img element by creating a variable img2 and appended it to the DOM

--- js/script.js
+++ js/script.js
@@ -73,9 +73,9 @@ const body = document.querySelector('body');
 
   // 4e. Append the `IMG` element to the `body` element
 
-  let img2 = document.createElement('img2');
-  document.getElementById('img2').src = 'img/mike.png';
-  document.getElementsById('img2').alt = 'Mike The Frog';
-  document.getElementById('img2').id = 'mike';
-  document.getElementsByClassName('img2').className = 'mike';
-  document.body.appendChild('img2');
+  let img2 = document.createElement('img');
+  img2.src = 'img/mike.png';
+  img2.alt = 'Mike The Frog';
+  img2.id = 'mike';
+  img2.className = 'mike';
+  document.body.appendChild(img2);
